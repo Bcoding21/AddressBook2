52b191009be13fe156de8ccf63ac63483b5b12cd
Brandon <brandon.cole@bison.howard.edu>
2018-05-22 15:24:16 -0400

    Added starting code

---
+++ Person.cpp
@@ -0,0 +1,33 @@
+#include "Person.h"
+
+
+std::string NO_VAL = "empty";
+
+Person::Person():
+	fName(NO_VAL),
+	lName(NO_VAL),
+	phoneNumber(NO_VAL)
+{
+}
+
+Person::Person(std::string first, std::string second, std::string num) :
+	fName(first),
+	lName(second),
+	phoneNumber(num)
+{
+}
+
+Person::Person(const Person& p) :
+	fName(p.fName),
+	lName(p.lName),
+	phoneNumber(p.phoneNumber)
+{
+}
+
+std::string Person::getFirstName() const { return fName; }
+
+std::string Person::getLastName() const { return lName; }
+
+std::string Person::getPhoneNumber() const { return phoneNumber; }
+
+
---
+++ Person.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <string>
+class Person
+{
+
+private:
+	const static std::string NO_VAL;
+	std::string fName;
+	std::string lName;
+	std::string phoneNumber;
+
+public:
+	Person();
+
+	Person(const Person&);
+
+	Person(std::string, std::string, std::string);
+
+	std::string getFirstName() const;
+
+	std::string getLastName() const;
+
+	std::string getPhoneNumber() const;
+	
+};
+
